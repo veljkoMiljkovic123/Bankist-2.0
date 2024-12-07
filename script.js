2681413dc4d960b4b9324555d0b6f498a7b85f4a
veljkoMiljkovic123 <mobilityworkout@gmail.com>
2024-12-07 17:28:38 +0100

    building a tabbed component and navbar items fade effect

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const btnScroll = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
 ///////////////////////////////////////
 // Modal window
 
@@ -29,11 +32,86 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+btnScroll.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Page navigation
+
+/* document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+}); */
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  //guard clause
+  if (!clicked) return;
+
+  //remove active classes
+  tabs.forEach(t => t.classList.remove('.operations__tab--active'));
+  tabsContent.forEach(el => el.classList.remove('operations__content--active'));
+
+  //activate tab
+  clicked.classList.add('operations__tab--active');
+
+  //activate content
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+//Menu fade animation
+const nav = document.querySelector('.nav');
+nav.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = '0.5';
+    });
+    logo.style.opacity = '0.5';
+  }
+});
+nav.addEventListener('mouseout', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = '1';
+    });
+    logo.style.opacity = '1';
+  }
+});
 //////////////////////////////////////////////////////
 //////////////////////////////////////////////////////
 //////////////////////////////////////////////////
 
-console.log(document.documentElement);
+/* console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 console.log(document.querySelector('html'));
@@ -86,4 +164,92 @@ console.log(link.href);
 console.log(link.getAttribute('href'));
 
 // Data attributes
-console.log(logo.dataset.versionNumber);
+console.log(logo.dataset.versionNumber); */
+
+/* const btnScroll = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1'); */
+/* btnScroll.addEventListener('click', function (e) { */
+/*   const s1cords = section1.getBoundingClientRect();
+  console.log(s1cords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log(`Currrent scrol (x/y)`, pageXOffset, pageYOffset);
+
+  console.log(
+    'Height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  ); */
+//top- position from the top of the viewport
+/* window.scrollTo(
+    s1cords.left + window.pageXOffset,
+    s1cords.top + window.pageYOffset
+  ); */
+
+/*   window.scrollTo({
+    left: s1cords.left + window.pageXOffset,
+    top: s1cords.top + window.pageYOffset,
+    behavior: 'smooth',
+  }); */
+
+/*   section1.scrollIntoView({ behavior: 'smooth' });
+}); */
+
+/* const h1 = document.querySelector('h1');
+const alertH1 = function (e) {
+  alert('addEventListener:Great! You are reading the heading');
+  setTimeout(() => {
+    h1.removeEventListener('mouseenter', alertH1);
+  }, 3000);
+};
+h1.addEventListener('mouseenter', alertH1);
+
+function myFunction() {
+  document.querySelector('h1').style.color = 'red';
+} */
+
+/* const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('LINK', e.target);
+  },
+  true
+);
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target);
+});
+document.querySelector('.nav ').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target);
+});
+ */
+
+/* const h1 = document.querySelector('h1');
+// Going downwards:child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'red';
+
+//Going upwards: parenta
+console.log(h1.parentElement);
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+//Going sideways: siblings
+console.log(h1.previousSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(el =>
+  el !== h1 ? (el.style.transform = scale(0.5)) : ''
+);
+ */
